b01170c459b3b7dbf5fecf5782b8b7029c505661
jjx <108453955+jjxws@users.noreply.github.com>
2026-06-02 13:51:46 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,4 @@
-export async function onRequest({request,next}){
-  const url = new URL(request.url);
-  if(url.pathname.startsWith("/api/")){
-    const auth = request.headers.get("Authorization");
-    if(auth !== "admin_ok") return new Response("禁止访问",{status:401})
-  }
+// functions/_middleware.js
+export async function onRequest({ next }) {
   return next();
 }
