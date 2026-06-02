36ce33ae5386a0c3978a7c84815bc977ecc07381
jjx <108453955+jjxws@users.noreply.github.com>
2026-06-02 13:46:31 +0800

    Update products.js

--- functions/api/products.js
+++ functions/api/products.js
@@ -1,14 +1,3 @@
-export async function onRequestGet({ env }) {
-  const data = await env.KV.get("products", { type: "json" }) || []
-  return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" }
-  })
-}
-
-export async function onRequestPost({ request, env }) {
-  const body = await request.json()
-  const list = await env.KV.get("products", { type: "json" }) || []
-  list.push(body)
-  await env.KV.put("products", JSON.stringify(list))
-  return new Response("OK")
+export async function onRequest({ next }) {
+  return next();
 }
