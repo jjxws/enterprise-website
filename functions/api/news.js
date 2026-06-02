d83777c7408232d18ab919c5101911bd43330d7b
jjx <108453955+jjxws@users.noreply.github.com>
2026-06-02 14:15:05 +0800

    Update news.js

--- functions/api/news.js
+++ functions/api/news.js
@@ -1,14 +1,23 @@
 export async function onRequestGet({ env }) {
-  const data = await env.KV.get("news", { type: "json" }) || []
+  const data = await env.KV.get("news", { type: "json" }) || [];
   return new Response(JSON.stringify(data), {
     headers: { "Content-Type": "application/json" }
-  })
+  });
 }
 
 export async function onRequestPost({ request, env }) {
-  const body = await request.json()
-  const list = await env.KV.get("news", { type: "json" }) || []
-  list.push(body)
-  await env.KV.put("news", JSON.stringify(list))
-  return new Response("OK")
+  const body = await request.json();
+  
+  if (body.del === 1) {
+    const idx = body.idx;
+    const list = await env.KV.get("news", { type: "json" }) || [];
+    list.splice(idx, 1);
+    await env.KV.put("news", JSON.stringify(list));
+    return new Response("OK");
+  }
+
+  const list = await env.KV.get("news", { type: "json" }) || [];
+  list.push(body);
+  await env.KV.put("news", JSON.stringify(list));
+  return new Response("OK");
 }
